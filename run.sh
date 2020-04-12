901469f3c9b6be6bb03182c04598217147a2f11e
edwinb-ai <developeredwin@gmail.com>
2020-04-12 15:50:26 -0500

    add debugging flags

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-nvcc langevin_hs.cu functionsl.cu -o browniano;
+nvcc -g -G langevin_hs.cu functionsl.cu -o browniano;
 # time ./browniano 0.40 10000 50000 0.00001 12398765 0 gr_1.dat;
 # ./a.out 0.40 0 100000 0.00001 12398765 1 gr_1.dat
 # ./a.out 0.40 0 100000 0.00001 32497068 1 gr_2.dat
