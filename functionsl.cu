76d5b03b1d98247bc2f794e23b70daba82847487
edwinb-ai <developeredwin@gmail.com>
2020-04-12 19:29:18 -0500

    changes

--- functionsl.cu
+++ functionsl.cu
@@ -94,13 +94,13 @@ __global__ void rdf_force(float *x, float *y, float *z, float *fx, float *fy, fl
                 }
 
                 // Actualizar los valores de las fuerzas
-                // fx[i] -= (fij * xij) / rij;
-                // fy[i] -= (fij * yij) / rij;
-                // fz[i] -= (fij * zij) / rij;
+                fx[i] += (fij * xij) / rij;
+                fy[i] += (fij * yij) / rij;
+                fz[i] += (fij * zij) / rij;
 
-                fx[j] += (fij * xij) / rij;
-                fy[j] += (fij * yij) / rij;
-                fz[j] += (fij * zij) / rij;
+                fx[j] -= (fij * xij) / rij;
+                fy[j] -= (fij * yij) / rij;
+                fz[j] -= (fij * zij) / rij;
                 ener = ener + uij;
                 // printf("%f\n", ener);
             }
