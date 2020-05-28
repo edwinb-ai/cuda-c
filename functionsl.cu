1c60f578243a4b06255a7150fa3df8316e860e9c
edwinb-ai <developeredwin@gmail.com>
2020-05-28 15:29:40 -0500

    fixes

--- functionsl.cu
+++ functionsl.cu
@@ -73,9 +73,9 @@ void rdf_force(float *x, float *y, float *z, float *fx, float *fy, float *fz,
             fij = 0.0f;
 
             // Contribucion de pares
-            xij = x[i] - x[j];
-            yij = y[i] - y[j];
-            zij = z[i] - z[j];
+            xij = x[j] - x[i];
+            yij = y[j] - y[i];
+            zij = z[j] - z[i];
 
             // Condiciones de frontera
             xij -= box_l * roundf(xij / box_l);
